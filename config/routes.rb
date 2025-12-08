866f6b3c9ba7cfa53cd7e81a16e5b7e7f03a2592
Newburu <newburu@gmail.com>
2025-12-08 20:15:58 +0900

    feat: OmniAuthコールバックが正しく機能するように、devise_forのOmniAuthルートをロケールスコープ外に移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # OmniAuth routes must be outside the locale scope to handle callbacks correctly
+  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+    devise_for :users, skip: :omniauth_callbacks, controllers: { registrations: "users/registrations" }
 
     resources :annual_themes do
       member do
@@ -9,8 +12,8 @@ Rails.application.routes.draw do
       resources :monthly_goals
     end
 
-    resources :mandala_items, only: [:edit, :update]
-    resources :daily_tasks, only: [:index, :create, :update, :destroy] do
+    resources :mandala_items, only: [ :edit, :update ]
+    resources :daily_tasks, only: [ :index, :create, :update, :destroy ] do
       collection do
         get :calendar
       end
